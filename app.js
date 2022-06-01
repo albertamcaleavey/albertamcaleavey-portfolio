bfa02121bad63029b2aba98c7485a913c4db7d10
Alberta McAleavey <a.mcaleavey24@gmail.com>
2022-05-31 21:43:05 -0400

    select project sections to add animations using intersection observer api

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-const aboutSection = document.getElementById('about')
+// const aboutSection = document.getElementById('about')
 
+const observer = new IntersectionObserver(sections => {
+  sections.forEach(section => {
+    if (section.isIntersecting) {
+      section.target.classList.add('"animate__animated", "animate__slideInLeft"')
+    }
+  })
+});
+
+observer.observe(document.querySelector('.project-section'));
+// Tell the observer which elements to track
