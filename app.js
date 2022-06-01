2e648eea3adc572b0f6e9e30f1c3464c29dcbe07
Alberta McAleavey <a.mcaleavey24@gmail.com>
2022-05-31 22:19:22 -0400

    add fade in annimations to about section

--- app.js
+++ app.js
@@ -1,39 +1,59 @@
+const aboutMePicObserver = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("animate__animated", "animate__fadeInLeft", "animate__slow")
+    }
+  })
+})
+
+aboutMePicObserver.observe(document.querySelector('.about-img-container'))
+
+const aboutDescriptionPicObserver = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("animate__animated", "animate__fadeInRight", "animate__slow")
+    }
+  })
+})
+
+aboutDescriptionPicObserver.observe(document.querySelector('.about-description'))
+
 const taskFlowObserver = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
-      entry.target.classList.add("animate__animated", "animate__slideInLeft")
+      entry.target.classList.add("animate__animated", "animate__fadeInLeft", "animate__slow")
     }
   })
-});
+})
 
-taskFlowObserver.observe(document.getElementById('task-flow'));
+taskFlowObserver.observe(document.getElementById('task-flow'))
 
 const cacheObserver = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
-      entry.target.classList.add("animate__animated", "animate__slideInRight")
+      entry.target.classList.add("animate__animated", "animate__fadeInRight", "animate__slow")
     }
   })
-});
+})
 
-cacheObserver.observe(document.getElementById('cache'));
+cacheObserver.observe(document.getElementById('cache'))
 
 const movieBuffObserver = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
-      entry.target.classList.add("animate__animated", "animate__slideInLeft")
+      entry.target.classList.add("animate__animated", "animate__fadeInLeft", "animate__slow")
     }
   })
-});
+})
 
-movieBuffObserver.observe(document.getElementById('movie-buff'));
+movieBuffObserver.observe(document.getElementById('movie-buff'))
 
 const runTrackerObserver = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
-      entry.target.classList.add("animate__animated", "animate__slideInRight")
+      entry.target.classList.add("animate__animated", "animate__fadeInRight", "animate__slow")
     }
   })
-});
+})
 
-runTrackerObserver.observe(document.getElementById('run-tracker'));
+runTrackerObserver.observe(document.getElementById('run-tracker'))
