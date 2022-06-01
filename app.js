a718e1b5ef2a3ed9460a929680083ad35a680756
Alberta McAleavey <a.mcaleavey24@gmail.com>
2022-05-31 21:58:26 -0400

    add slide in annimations to project sections when visibile

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
-// const aboutSection = document.getElementById('about')
-
-const observer = new IntersectionObserver(sections => {
-  sections.forEach(section => {
-    if (section.isIntersecting) {
-      section.target.classList.add('"animate__animated", "animate__slideInLeft"')
+const taskFlowObserver = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("animate__animated", "animate__slideInLeft")
     }
   })
 });
 
-observer.observe(document.querySelector('.project-section'));
-// Tell the observer which elements to track
+taskFlowObserver.observe(document.getElementById('task-flow'));
+
+const cacheObserver = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("animate__animated", "animate__slideInRight")
+    }
+  })
+});
+
+cacheObserver.observe(document.getElementById('cache'));
+
+const movieBuffObserver = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("animate__animated", "animate__slideInLeft")
+    }
+  })
+});
+
+movieBuffObserver.observe(document.getElementById('movie-buff'));
+
+const runTrackerObserver = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("animate__animated", "animate__slideInRight")
+    }
+  })
+});
+
+runTrackerObserver.observe(document.getElementById('run-tracker'));
